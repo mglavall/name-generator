69764807c5c7ccbecf46141df6112fef681a7ca2
mglavall <mgomezlavall@gmail.com>
2018-03-04 20:09:52 +0100

    new project: marsa name generator

--- controller.js
+++ controller.js
@@ -1,37 +1,30 @@
-var countDownDate = new Date("Nov 20, 2017 00:00:00").getTime();
-
-var x = setInterval(function() {
-
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-}, 1000);
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshNormalMaterial();
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function animate() {
-	requestAnimationFrame( animate );
-  cube.rotation.x += 0.01;
-cube.rotation.y += 0.02;
-	renderer.render( scene, camera );
+var names = [
+"Marçal",
+"Marsa",
+"Marsa la Comparsa",
+"Marsupesa",
+"Marsi",
+"Marsa Calamarsa",
+"Marsu",
+"La Marsellesa",
+"Marsupas",
+"Marsupilami",
+"Marsa ttack",
+"Marcelo",
+"Marcelino",
+"DJ Marsa",
+"Bruno Marsa",
+"El bueno de Marsi",
+]
+var name = "Marçal";
+function getRandomName(){
+  var aux = false;
+  while(!aux){
+    var newName = names[Math.floor(Math.random()*names.length)];
+    if(newName != name){
+      aux = true;
+    }
+  }
+ $("#name").text(newName);
+ name = newName;
 }
-animate();
